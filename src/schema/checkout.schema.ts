b5036920000fc978cac62ce1195273b2af4c4e03
nikul6 <nikuldhameliya6@gmail.com>
2023-07-19 18:57:55 +0530

    Delivery screen validate

--- src/schema/checkout.schema.ts
+++ src/schema/checkout.schema.ts
@@ -6,3 +6,12 @@ export const PersonalInfoSchema = z.object({
 });
 
 export type PersonalInfo = z.infer<typeof PersonalInfoSchema>;
+
+export const DeliveryInfoSchema = z.object({
+    city: z.string().min(1),
+    postalCode: z.string(),
+    address: z.string(),
+    shipping: z.enum(['free', 'fast', 'same_day'])
+});
+
+export type DeliveryInfo = z.infer<typeof DeliveryInfoSchema>;
